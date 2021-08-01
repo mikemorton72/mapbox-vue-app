a9ed75be62c1abd16aec20f0b8c729d79339cbbf
Mike Morton <mmorton7295@gmail.com>
2021-08-01 10:39:19 -0500

    adds Map Box to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="map" style="width: 800px; height: 450px"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  mounted: function () {
+    this.doMapBox();
+  },
+  methods: {
+    doMapBox: function () {
+      mapboxgl.accessToken = process.env.VUE_APP_MAP_BOX_API_KEY;
+      const map = new mapboxgl.Map({
+        container: "map", // container ID
+        style: "mapbox://styles/mapbox/streets-v11", // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+      });
+    },
   },
 };
 </script>
